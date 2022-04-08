e3c5eba01d91904df068325d1d99704de380c9e7
Sasha <aleks-one@ukr.net>
2022-04-08 21:59:22 +0300

    Change some style, improved data output from loop for changecolor function

--- js/index.js
+++ js/index.js
@@ -4,17 +4,54 @@ fetch('http://localhost:3005')
     })
     .then((data) => {
         console.log(data);
-        const result = data.map(x => 
-            `<li class="main_item">${x.question}<span class="btns-wrap"><button class="btn">&#10004;</button><button class="btn">&#10008;</button><button class="btn so-so-btn">&plusmn;</button></span></li>`)
-            .join('');
-        document.querySelector('.main').innerHTML = result;
-    });
+        data.forEach(function (x) {
+            document.querySelector('.main').insertAdjacentHTML(
+                "beforeend",
+                `<li class="main_item">${x.question}
+                <span id="btns">
+                <button class="btn-ok">&#10004;</button>
+                <button class="btn-not-ok">&#10008;</button>
+                <button class="btn-so-so">&plusmn;</button>
+                </span>
+                </li>`
+            );
+        });
+        // document.querySelector('.btn').addEventListener('click', function(){
+        //     document.querySelector('.main_item').style.color = 'green';
+        // });
+        // btns.onclick = function(event) {  
+        //     if (event.target.className != 'btn' ) return;
+        //     document.querySelector('.main_item').style.color = 'green';
+        // }
+        function changeColor(a, b, color) {
+            let elements = document.querySelectorAll(a);
+            // for (let i = 0; i < elements.length; i++) {
+            //     elements[i].onclick = function() {
+            //     let el = document.querySelectorAll(b);
+            //         for (let v = 0; v < el.length; v++) {
+            //             el[v].style.color = color;
+            //         };
+            //     };
+            // };
+            elements.forEach(function (x) {
+                x.onclick = function() {
+                    let el = document.querySelectorAll(b);
+                    el.forEach(x => x.style.color = color);
+                    // let item = document.querySelectorAll(a);
+                    // item.forEach(x => {
+                    //     x.style.color = color;
+                    //     x.style.fontSize = '25px'
+                    // });
+
+                    // let item = document.querySelector(a);
+                    // item.style.color = color;
+                };
+            });
+        };
+        changeColor('.btn-ok', '.main_item', '#03914e');
+        changeColor('.btn-not-ok', '.main_item', '#bd0000');
+        changeColor('.btn-so-so', '.main_item', '#e9ab00');
+    });         
 
 
-    // const some = document.querySelector('.header');
-    // some.style.backgroundColor = 'red';
-
-
-// document.querySelector('.btn').addEventListner('click', () => {
-//     document.querySelector('.main_item').style.color = 'red'
-// })
+// document.li.className.add('main_item');
