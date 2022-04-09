9343160bf77e451ef01efe897955544e97852f66
Sasha <aleks-one@ukr.net>
2022-04-09 21:06:30 +0300

    Added a function to check the status of the question and its stylization, added function to change color of buttons

--- js/index.js
+++ js/index.js
@@ -5,17 +5,42 @@ fetch('http://localhost:3005')
     .then((data) => {
         console.log(data);
         data.forEach(function (x) {
-            document.querySelector('.main').insertAdjacentHTML(
-                "beforeend",
-                `<li class="main_item">${x.question}
-                <span id="btns">
-                <button class="btn-ok">&#10004;</button>
-                <button class="btn-not-ok">&#10008;</button>
-                <button class="btn-so-so">&plusmn;</button>
-                </span>
-                </li>`
-            );
+            function render(clss) {
+                document.querySelector('.main').insertAdjacentHTML(
+                    "beforeend",
+                    `<li class="main_item ${clss}">${x.question}
+                    <span id="btns">
+                    <button class="btn-ok">&#10004;</button>
+                    <button class="btn-not-ok">&#10008;</button>
+                    <button class="btn-so-so">&plusmn;</button>
+                    </span>
+                    </li>`
+                );
+            };           
+            if (x.status == 1) {
+                render('main_item-status1');
+            } else if (x.status == 2) {
+                render('main_item-status2');
+            } else if (x.status == 3) {
+                render('main_item-status3');
+            } else {
+                render('')
+            };   
         });
+
+        function clickBtn(clss, color) {
+            let elements = document.querySelectorAll(clss);
+            elements.forEach(function(x) {
+                x.onclick = function() {
+                    this.style.color = color;
+                }; 
+            });
+        };
+
+        clickBtn('.btn-ok', '#03914e');
+        clickBtn('.btn-not-ok', '#bd0000');
+        clickBtn('.btn-so-so', '#e9ab00');
+
         // document.querySelector('.btn').addEventListener('click', function(){
         //     document.querySelector('.main_item').style.color = 'green';
         // });
@@ -23,34 +48,22 @@ fetch('http://localhost:3005')
         //     if (event.target.className != 'btn' ) return;
         //     document.querySelector('.main_item').style.color = 'green';
         // }
-        function changeColor(a, b, color) {
-            let elements = document.querySelectorAll(a);
-            // for (let i = 0; i < elements.length; i++) {
-            //     elements[i].onclick = function() {
-            //     let el = document.querySelectorAll(b);
-            //         for (let v = 0; v < el.length; v++) {
-            //             el[v].style.color = color;
-            //         };
-            //     };
-            // };
-            elements.forEach(function (x) {
-                x.onclick = function() {
-                    let el = document.querySelectorAll(b);
-                    el.forEach(x => x.style.color = color);
-                    // let item = document.querySelectorAll(a);
-                    // item.forEach(x => {
-                    //     x.style.color = color;
-                    //     x.style.fontSize = '25px'
-                    // });
 
-                    // let item = document.querySelector(a);
-                    // item.style.color = color;
-                };
-            });
-        };
-        changeColor('.btn-ok', '.main_item', '#03914e');
-        changeColor('.btn-not-ok', '.main_item', '#bd0000');
-        changeColor('.btn-so-so', '.main_item', '#e9ab00');
+        // function changeColor(a, b, color) {
+        //     let elements = document.querySelectorAll(a);
+        //     elements.forEach(function (x) {
+        //         x.onclick = function() {
+        //             this.style.color = color;
+        //             this.style.fontSize = '25px'
+        //             let el = document.querySelectorAll(b);
+        //             el.forEach(x => x.style.color = color);
+        //         };
+        //     });
+        // };
+        
+        // changeColor('.btn-ok', '.main_item', '#03914e');
+        // changeColor('.btn-not-ok', '.main_item', '#bd0000');
+        // changeColor('.btn-so-so', '.main_item', '#e9ab00');
     });         
 
 
